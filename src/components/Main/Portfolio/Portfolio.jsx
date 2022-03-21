968cc8113af5e5b0aa8922b2b0c2ba62747ce36d
leviterante <terantelevi@gmail.com>
2022-03-21 17:49:35 +0100

    add portfolios

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { GiSheikahEye } from "react-icons/gi";
+import { AiOutlineLink } from "react-icons/ai";
+import Isotope from "isotope-layout";
+
+import { images } from "../../../constants";
 
 const Portfolio = () => {
+  // state for storing the isotope object, with an initial value of null
+  const [isotope, setIsotope] = useState(null);
+  // state for storing the filter keyword, with an initial value of *, which matches everything
+  const [filterKey, setFilterKey] = useState("*");
+  useEffect(() => {
+    setIsotope(
+      new Isotope(".portfolio-container", {
+        // filter-container: className of the parent of the isotope elements
+        itemSelector: ".portfolio-item", // filter-item: className of the isotope elements
+        layoutMode: "fitRows",
+      })
+    );
+  }, []); // [] makes this useEffect work like a componentDidMount in a class component
+  useEffect(() => {
+    if (isotope) {
+      // sanity check
+      filterKey === "*"
+        ? isotope.arrange({ filter: `*` })
+        : isotope.arrange({ filter: `.${filterKey}` });
+    }
+  }, [isotope, filterKey]);
+
   return (
     <section id="portfolio" className="portfolio section-bg">
       <div className="container">
@@ -16,12 +43,12 @@ const Portfolio = () => {
         <div className="row" data-aos="fade-up">
           <div className="col-lg-12 d-flex justify-content-center">
             <ul id="portfolio-filters">
-              <li data-filter="*" className="filter-active">
+              <li onClick={() => setFilterKey("*")} className="filter-active">
                 All
               </li>
-              <li data-filter=".filter-app">App</li>
-              <li data-filter=".filter-card">Card</li>
-              <li data-filter=".filter-web">Web</li>
+              <li onClick={() => setFilterKey("filter-app")}>App</li>
+              <li onClick={() => setFilterKey("filter-card")}>Card</li>
+              <li onClick={() => setFilterKey("filter-web")}>Web</li>
             </ul>
           </div>
         </div>
@@ -33,10 +60,45 @@ const Portfolio = () => {
         >
           <div className="col-lg-4 col-md-6 portfolio-item filter-app">
             <div className="portfolio-wrap">
-              <img src="" alt="" />
+              <img src={images.portfolio1} className="img-fluid" alt="" />
               <div className="portfolio-links">
-                <a href=""></a>
-                <a href=""></a>
+                <a
+                  href={images.portfolio1}
+                  data-gallery="portfolioGallery"
+                  className="portfolio-lightbox"
+                  title="App 1"
+                >
+                  <i>
+                    <GiSheikahEye />
+                  </i>
+                </a>
+                <a href="/" title="More Details">
+                  <i>
+                    <AiOutlineLink />
+                  </i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div className="portfolio-wrap">
+              <img src={images.rain1} className="img-fluid" alt="" />
+              <div className="portfolio-links">
+                <a
+                  href={images.rain1}
+                  data-gallery="portfolioGallery"
+                  className="portfolio-lightbox"
+                  title="App 1"
+                >
+                  <i>
+                    <GiSheikahEye />
+                  </i>
+                </a>
+                <a href="/" title="More Details">
+                  <i>
+                    <AiOutlineLink />
+                  </i>
+                </a>
               </div>
             </div>
           </div>
--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -1,3 +1,17 @@
 import profile from "../assets/img/profile-img.jpg";
+import portfolio1 from "../assets/img/web_portfolio(1).png";
+import portfolio2 from "../assets/img/web_portfolio(2).png";
+import portfolio3 from "../assets/img/web_portfolio(3).png";
+import rain1 from "../assets/img/rain(1).png";
+import rain2 from "../assets/img/rain(2).png";
+import p5 from "../assets/img/p5_JS.png";
 
-export default { profile };
+export default {
+  profile,
+  portfolio1,
+  portfolio2,
+  portfolio3,
+  rain1,
+  rain2,
+  p5,
+};
